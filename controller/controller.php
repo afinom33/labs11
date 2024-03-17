ae119e668cf1a721cc95d401d70e002cd72d7554
afinom33 <rick.sanchez.81@mail.ru>
2024-03-17 12:24:25 +0900

    fifth lab done

--- controller/controller.php
+++ controller/controller.php
@@ -4,15 +4,13 @@ defined('ISHOP') or die('Access denied');
 
 require_once MODEL;
 
-<<<<<<< HEAD
 require_once 'functions/functions.php';
 
 $cat = catalog();
 
 $view = empty($_GET['view']) ? 'hits' : $_GET['view'];
     
-=======
 $view = empty($_GET['view']) ? 'hits' : $_GET['view'];
->>>>>>> 218b2a7325e0422a5eec39a52ce79489c7bd9a63
+
 
 require_once TEMPLATE.'index.php';
