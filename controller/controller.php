7e76dc5b16c17e50aa2ec567e32ddded7a459cf6
afinom33 <rick.sanchez.81@mail.ru>
2024-03-17 12:22:39 +0900

    fifth lab done

--- controller/controller.php
+++ controller/controller.php
@@ -4,13 +4,15 @@ defined('ISHOP') or die('Access denied');
 
 require_once MODEL;
 
-$view = empty($_GET['view']) ? 'hits' : $_GET['view'];
-
+<<<<<<< HEAD
 require_once 'functions/functions.php';
 
 $cat = catalog();
 
 $view = empty($_GET['view']) ? 'hits' : $_GET['view'];
     
+=======
+$view = empty($_GET['view']) ? 'hits' : $_GET['view'];
+>>>>>>> 218b2a7325e0422a5eec39a52ce79489c7bd9a63
 
 require_once TEMPLATE.'index.php';
---
+++ views/ishop/sale.php
@@ -0,0 +1,27 @@
+<<<<<<< HEAD
+=======
+
+>>>>>>> 218b2a7325e0422a5eec39a52ce79489c7bd9a63
+<?php defined('ISHOP') or die('Access denied'); ?>
+<div class="catalog-index">
+				<h1> Распродажа </h1>
+				<div class="product-index">
+<<<<<<< HEAD
+					<h2><a href="#">Sony Xperia S 32Gb (черный)</a></h2>
+					<a href="#"><img src="<?=TEMPLATE?>images/phone-index.jpg" alt="" /></a>
+					<p>Цена:  <span>24 990.</span></p>
+					<a href="#"><img class="addtocard-index" src="<?=TEMPLATE?>images/addcard-index.jpg" 
+					alt="Добавить в корзину" /></a>
+				</div>
+			</div>
+=======
+				
+					<h2><a href="#">Sony Xperia S 32Gb (черный)</a></h2>
+					<a href="#"><img src="<?=TEMPLATE?>images/phone-index.jpg" alt="" /></a>
+					<p>Цена :  <span>24 990.</span></p>
+					<a href="#"><img class="addtocard-index" src="<?=TEMPLATE?>images/addcard-index.jpg" 
+					alt="Добавить в корзину" /></a>
+				</div>
+				
+			</div>
+>>>>>>> 218b2a7325e0422a5eec39a52ce79489c7bd9a63
